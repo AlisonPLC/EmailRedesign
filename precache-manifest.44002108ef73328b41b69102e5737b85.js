5ee126958ba93907d2053dea3c9368267f70f09e
Alison PLC <alisonpietro@gmail.com>
2020-04-07 21:46:19 -0300

    Updates

---
+++ precache-manifest.44002108ef73328b41b69102e5737b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd238fba49a863c469262d2c10cc759",
+    "url": "/EmailRedesign/index.html"
+  },
+  {
+    "revision": "69b23ba2a30a7e66cb4b",
+    "url": "/EmailRedesign/static/css/main.eafa4f80.chunk.css"
+  },
+  {
+    "revision": "d59e8c9c4138dc3875af",
+    "url": "/EmailRedesign/static/js/2.c303e35d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmailRedesign/static/js/2.c303e35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b23ba2a30a7e66cb4b",
+    "url": "/EmailRedesign/static/js/main.d63ac9b1.chunk.js"
+  },
+  {
+    "revision": "2f2e45551a9ebdeb924b",
+    "url": "/EmailRedesign/static/js/runtime-main.3bb842f9.js"
+  },
+  {
+    "revision": "53d29e80bab77c5d784f9b55c7451458",
+    "url": "/EmailRedesign/static/media/gmail-logo-1.53d29e80.png"
+  }
+]);
